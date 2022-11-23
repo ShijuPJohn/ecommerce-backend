79f2f164d826073b60642d2f9a27ff9c82ed8117
ShijuPJohn <shijupjohn4u@gmail.com>
2022-11-23 21:58:54 +0530

    add product model and add it to the admin panel

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=256, null=True, blank=True)
+    brand = models.CharField(max_length=256, null=True, blank=True)
+    # image = models.CharField(max_length=25, null=True, blank=True)
+    category = models.CharField(max_length=25, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    num_reviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    count_in_stock = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
