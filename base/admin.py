d281f28e792c80398aecdc430c443f500b918417
ShijuPJohn <shijupjohn4u@gmail.com>
2022-11-24 11:41:25 +0530

    add image field to product. install pillow. complete add models

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from base.models import Product
+from base.models import *
 
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(Review)
