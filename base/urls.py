cbfb04933b702b3e23118e009ee25d7a4674d7f0
ShijuPJohn <shijupjohn4u@gmail.com>
2022-11-28 20:54:47 +0530

    add simple jwt authentication - in progress

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from base.views import get_products, get_routes, get_product_details
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 urlpatterns = [
     path('', get_routes, name='routes'),
     path('products', get_products, name='products'),
     path('products/<str:pid>', get_product_details, name='product_details'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
