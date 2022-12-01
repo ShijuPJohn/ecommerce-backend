352425a056bc9f77b758a8467550c7a2badf4df8
ShijuPJohn <shijupjohn4u@gmail.com>
2022-12-01 21:11:17 +0530

    add signals function to set username as email

--- base/signals.py
+++ base/signals.py
@@ -3,7 +3,9 @@ from django.db.models.signals import pre_save
 
 
 def update_user(sender, instance, **kwargs):
-    print('-------------------------------signal triggered')
+    user = instance
+    if user.email != '':
+        user.username = user.email
 
 
 pre_save.connect(update_user, sender=User)
