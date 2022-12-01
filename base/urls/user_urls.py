a5befbf895ff2ec7a7b16e1c7f16e5efed39d735
ShijuPJohn <shijupjohn4u@gmail.com>
2022-12-01 21:38:33 +0530

    refactor urls and views into separate folders and files

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from base.views.user_views import MyTokenObtainPairView, get_user_profile, get_users, register_user
+from base.views.user_views import *
 
 urlpatterns = [
-    path('users/login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/user', get_user_profile, name='user profile'),
-    path('users', get_users, name="users"),
-    path('users/register', register_user, name="register user")
+    path('/login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/user', get_user_profile, name='user profile'),
+    path('', get_users, name="users"),
+    path('/register', register_user, name="register user")
 ]
