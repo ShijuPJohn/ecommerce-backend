543c4abc860792759d36a7aa657547412e729d83
ShijuPJohn <shijupjohn4u@gmail.com>
2023-01-02 00:27:29 +0530

    unknown changes. to push

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -4,5 +4,5 @@ from base.views.product_views import get_product_details, get_products
 
 urlpatterns = [
     path('', get_products, name='products'),
-    path('/<str:pid>', get_product_details, name='product_details'),
+    path('<str:pid>', get_product_details, name='product_details'),
 ]
--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from base.views.user_views import *
 
 urlpatterns = [
-    path('/login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('/user', get_user_profile, name='user profile'),
+    path('login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user', get_user_profile, name='user profile'),
     path('', get_users, name="users"),
-    path('/register', register_user, name="register user")
+    path('register', register_user, name="register user")
 ]
